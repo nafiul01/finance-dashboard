902677128750bd0602c8b64c12ce2b7addecf0d6
nafiul01 <nafiul01@gmail.com>
2022-06-09 01:55:55 +0600

    Responsive Finance Website without mobile navbar

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      colors:{
+        bColor:'#4C49ED',
+        bsColor:'#AFAEFE',
+        gColor:'#4FD18B',
+        gsColor:'#DCF5E8',
+        rColor:'#D14F4F',
+        rsColor:'#F5DCDC',
+        dbColor:'#141197',
+        grColor:'#E0DFF0',
+        teColor:'#A1A0BD',
+        mvColor:'#E4E3FF',
+      },
+    },
+  },
+  plugins: [],
+}
