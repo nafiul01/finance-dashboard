b97f5d97271e33f65441fe94f6ec262b7b4cdf2e
nafiul01 <nafiul01@gmail.com>
2022-06-21 18:20:23 +0600

    Color & Font Update
    1. Changed and rename the global colors and applied it to the html.
    2. Added font to global tailwindcss.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,17 +3,18 @@ module.exports = {
   theme: {
     extend: {
       colors:{
-        bColor:'#4C49ED',
-        bsColor:'#AFAEFE',
-        gColor:'#4FD18B',
-        gsColor:'#DCF5E8',
-        rColor:'#D14F4F',
-        rsColor:'#F5DCDC',
-        dbColor:'#141197',
-        grColor:'#E0DFF0',
-        teColor:'#A1A0BD',
-        mvColor:'#E4E3FF',
+        primaryColor: '#4C49ED',
+        secondaryColor: '#A1A0BD',
+        secondaryColorShade: '#d1d5db',
+        greenColor: '#4FD18B',
+        greenColorShade: '#DCF5E8',
+        redColor: '#D14F4F',
+        redColorShade: '#F5DCDC',
+        backgroundColor: '#F4F3FA',
       },
+      fontFamily:{
+        'mulish': 'Mulish',
+      }
     },
   },
   plugins: [],
